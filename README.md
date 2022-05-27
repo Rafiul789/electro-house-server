ecccdc5bdf32afaacc40d6c49a5b05b47eabc8e2
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-05-27 14:45:53 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,25 @@
+## Site Name :Electro House
+
+### Live Link: 
+
+### Server Link: 
+
+## `Top Features`
+
+1. In Home Page their is navbar,banner,product,faqs,business summary ,reviews and footer section. 
+2. By Clicking the purchase button,user will go to purchase page.Here user can order their desired products.
+3. They can see their orders in my orders page.
+
+4. If users are logged in, they will see dashboad and add reviews pages.User can add reviews in reviews page.
+
+5. Login authorization added using firebase.
+
+
+## `Tech Stack `
+
+1. React 
+2.React hook,React Hook Form
+2. Tailwind CSS 
+3. Node and Express JS
+4. MongoDB 
+5. DaisyUI
--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
 async function run(){
 try{
 
@@ -38,8 +39,9 @@ res.send(product);
     });
 
     app.get('/order',async(req,res)=>{
-      const orders=await orderCollection.find().toArray();
-      res.send(orders)
+     const orders=await orderCollection.find().toArray();
+       res.send(orders)
+    
     })
 
     app.post('/order',async(req,res) =>{
@@ -62,9 +64,7 @@ res.send({success:true,result})
     })
     app.put('/user/admin/:email',async(req,res)=>{
       const email=req.params.email;
-     
       const filter={email:email}
-     
       const updateDoc = {
        $set:{role:'admin'},
      };
